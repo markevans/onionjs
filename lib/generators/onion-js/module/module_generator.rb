c69121ae4cd0fff3c6eff72006854dc026463e1f
Mark Evans <mark@new-bamboo.co.uk>
2013-03-01 16:31:09 +0000

    moved generator into onion-js folder and renamed to onion:module

--- lib/generators/onion-js/module/module_generator.rb
+++ lib/generators/onion-js/module/module_generator.rb
@@ -1,5 +1,5 @@
 module Js
-  class ControllerGenerator < ::Rails::Generators::NamedBase
+  class ModuleGenerator < ::Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
     argument :name, :type => :string
