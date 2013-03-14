852ff29815195fb0792e43352bee1c56e938e973
Mark Evans <mark@new-bamboo.co.uk>
2013-03-14 15:30:32 +0000

    Added pre_require option to app helper

--- app/helpers/onionjs/helper.rb
+++ app/helpers/onionjs/helper.rb
@@ -4,14 +4,16 @@ module Onionjs
     def onionjs_app(app_name, preloaded_data={}, opts={})
       app_path = opts[:app_path] || "#{app_name}/#{app_name}_controller"
       id = (opts[:id] || "app").html_safe
+      pre_require = opts[:pre_require]
 
+      requires = [app_path] + Array.wrap(pre_require)
       controller_name = "#{app_name}_controller".camelize
 
       html = ""
 
       html << %(
         <script type="text/javascript">
-          require(['#{app_path}'], function(#{controller_name}){
+          require(#{requires.to_json}, function(#{controller_name}){
             window.app = new #{controller_name}({
               preloadedData: #{preloaded_data.to_json}
             }).appendTo('##{id}')
