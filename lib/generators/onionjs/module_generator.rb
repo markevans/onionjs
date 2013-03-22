bf121d1856c7e6dd0347267e23b9f04aac966d69
Mark Evans <mark@new-bamboo.co.uk>
2013-03-22 15:27:46 +0000

    defaults js generator but allows coffeescript through the --coffee flag

--- lib/generators/onionjs/module_generator.rb
+++ lib/generators/onionjs/module_generator.rb
@@ -3,10 +3,16 @@ module Onionjs
     source_root File.expand_path('../templates', __FILE__)
 
     argument :name, :type => :string
+    class_option :coffee, :type => :boolean, :desc => "generate coffeescript instead of javascript"
 
     def create_stuff
-      template "controller.coffee.erb", "app/assets/modules/#{module_dirname}/#{controller_basename}.coffee"
-      template "view.coffee.erb", "app/assets/modules/#{module_dirname}/#{view_basename}.coffee"
+      if options["coffee"]
+        template "controller.coffee.erb", "app/assets/modules/#{module_dirname}/#{controller_basename}.coffee"
+        template "view.coffee.erb", "app/assets/modules/#{module_dirname}/#{view_basename}.coffee"
+      else
+        template "controller.js.erb", "app/assets/modules/#{module_dirname}/#{controller_basename}.js"
+        template "view.js.erb", "app/assets/modules/#{module_dirname}/#{view_basename}.js"
+      end
       template "template.mustache.erb", "app/assets/modules/#{module_dirname}/#{template_filename}"
       template "view.css.scss.erb", "app/assets/modules/#{module_dirname}/#{stylesheet_filename}"
     end
