80de80afac7753b5b1c14311962b8c4417506556
Pablo Brasero <pablo@new-bamboo.co.uk>
2013-07-30 17:39:11 +0100

    More flexible Ruby helper

--- app/helpers/onionjs/helper.rb
+++ app/helpers/onionjs/helper.rb
@@ -1,9 +1,20 @@
 module Onionjs
   module Helper
 
-    def onionjs_app(app_name, preloaded_data={}, opts={})
+    def attach_onionjs_app(*args)
+      onionjs_app('attach', *args)
+    end
+
+    def append_onionjs_app(*args)
+      onionjs_app('append', *args)
+    end
+
+
+    private
+
+    def onionjs_app(action, app_name, preloaded_data={}, opts={})
       app_path = opts[:app_path] || "#{app_name}/#{app_name}_controller"
-      id = (opts[:id] || "app").html_safe
+
       pre_require = opts[:pre_require]
 
       requires = [app_path] + Array.wrap(pre_require)
@@ -16,15 +27,35 @@ module Onionjs
           require(#{requires.to_json}, function(#{controller_name}){
             window.app = new #{controller_name}({
               preloadedData: #{preloaded_data.to_json}
-            }).appendTo('##{id}')
+            })
+            window.app.#{action}To('#{onionjs_app_anchor_selector(opts)}')
+            window.app.run()
           })
         </script>
       )
 
-      html << %(<div id="#{id}"></div>)
+      (onionjs_app_anchor_element(opts) + html).html_safe
+    end
 
-      html.html_safe
+    def onionjs_app_anchor_selector(opts)
+      if opts[:base]
+        opts[:base]
+      elsif opts[:id]
+        '#' + opts[:id].to_s
+      else
+        '#app'
+      end
+    end
+
+    def onionjs_app_anchor_element(opts)
+      if opts[:base]
+        ''
+      else
+        id = opts[:id] || 'app'
+        %(<div id="#{id}"></div>)
+      end
     end
 
   end
 end
+
