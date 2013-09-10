5b7cc85d93b7858a684bf93959b7fad515314803
Mark Evans <mark@new-bamboo.co.uk>
2013-09-10 12:13:35 +0100

    UrlHashRouter#update and #setHash

--- vendor/assets/javascripts/onion/url_hash_router.js
+++ vendor/assets/javascripts/onion/url_hash_router.js
@@ -18,6 +18,15 @@ define([
       hash: function () {
         var matches = window.location.href.match(/#(.*)$/)
         return matches ? matches[1] : ""
+      },
+
+      setHash: function (hash) {
+        // Need to test this in IE7
+        window.location.hash = hash
+      },
+
+      update: function (name, params) {
+        this.setHash(this.path(name, params))
       }
 
     })
