544f712badf22640342eb7bef6c30121bac9e907
Mark Evans <mark@new-bamboo.co.uk>
2013-09-17 13:06:07 +0100

    allow adding "persistent params" to json API

--- vendor/assets/javascripts/onion/json_api.js
+++ vendor/assets/javascripts/onion/json_api.js
@@ -3,8 +3,9 @@ if(typeof define!=='function'){var define=require('amdefine')(module);}
 define([
   'jquery',
   'onion/type',
-  'onion/event_emitter'
-], function($, Type, eventEmitter){
+  'onion/event_emitter',
+  'onion/extend'
+], function($, Type, eventEmitter, extend){
 
   return Type.sub('JsonApi')
 
@@ -15,22 +16,23 @@ define([
         if (!options) options = {}
         this.urlPrefix = options.urlPrefix || ""
         this.ajaxOptions = options.ajaxOptions || {}
+        this.persistentParams = options.persistentParams || {}
       },
 
       get: function (url, data, options) {
-        return this.ajax('GET', url, data, options)
+        return this.ajax('GET', url, this.__addPersistentParams__(data), options)
       },
 
       put: function (url, data, options) {
-        return this.ajax('PUT', url, JSON.stringify(data), options)
+        return this.ajax('PUT', url, JSON.stringify(this.__addPersistentParams__(data)), options)
       },
 
       post: function (url, data, options) {
-        return this.ajax('POST', url, JSON.stringify(data), options)
+        return this.ajax('POST', url, JSON.stringify(this.__addPersistentParams__(data)), options)
       },
 
       delete: function (url, data, options) {
-        return this.ajax('DELETE', url, JSON.stringify(data), options)
+        return this.ajax('DELETE', url, JSON.stringify(this.__addPersistentParams__(data)), options)
       },
 
       ajax: function (type, url, data, options) {
@@ -43,6 +45,10 @@ define([
           contentType: 'application/json',
           processData: (type === 'GET' ? true : false)
         }, this.ajaxOptions, options.ajaxOptions))
+      },
+
+      __addPersistentParams__: function (data) {
+        return extend({}, this.persistentParams, data)
       }
     })
 
